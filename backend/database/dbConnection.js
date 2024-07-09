3d0fcd397859ed0a4cf026e06f42f4f35ea9528f
Titu Ray choudhury <tituraychoudhury@gmail.com>
2024-07-09 23:29:52 +0530

    resolve issue on database connection

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 export const dbConnection = ()=>{
-    mongoose.connect(process.env.MONGO_URI,{
+    mongoose.connect(process.env.MONGO_URL,{
         dbName: "HOSPITAL_MANAGEMENT_SYSTEM"
     })
     .then(()=>{
