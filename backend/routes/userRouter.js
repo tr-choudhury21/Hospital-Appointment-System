ecdc7233642eda6b9b1ce4b5693a3289c861ee20
Titu Ray choudhury <tituraychoudhury@gmail.com>
2024-07-10 23:25:20 +0530

    add authentication  & authorization

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { addNewAdmin, login, patientRegister } from "../controller/userController.js";
+import { isAdminAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/patient/register", patientRegister);
 router.post("/login", login);
-router.post("/admin/new", addNewAdmin);
+router.post("/admin/new", isAdminAuthenticated, addNewAdmin);
 
 export default router;
