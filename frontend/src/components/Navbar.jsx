d9d35e89f87b80cf08809c9c2bf591bc4d83abdd
Titu Ray choudhury <tituraychoudhury@gmail.com>
2024-07-16 23:55:21 +0530

    setup navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useContext, useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import { Context } from '../main';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { toast } from 'react-toastify';
+
+const Navbar = () => {
+
+    const [show, setShow] = useState(false);
+    const { isAuthenticated , setIsAuthenticated} = useContext(Context);
+
+    const handleLogout = async()=>{
+        await axios
+            .get("http://localhost:5000/api/v1/user/patient/logout", {
+            withCredentials: true,
+        })
+        .then((res) => {
+            toast.success(res.data.message);
+            setIsAuthenticated(false);
+        })
+        .catch((err) => {
+            toast.error(err.response.data.message);
+        });
+    };
+
+    const navigateTo = useNavigate();
+
+    const gotoLogin = async()=>{
+        navigateTo("/login");
+    };
+
+    return (
+        <nav className='container'>
+            <div className="logo">HeavenCare</div>
+            <div className={show ? "navLinks showmenu" : "navLinks"}>
+                <div className="links">
+                    <Link to={"/"} onClick={() => setShow(!show)}>Home</Link>
+                    <Link to={"/appointment"} onClick={() => setShow(!show)}>Appointment</Link>
+                    <Link to={"/about"} onClick={() => setShow(!show)}>About Us</Link>
+                </div>
+                {!isAuthenticated ? (
+                    <button className='logoutBtn btn' onClick={handleLogout}>Logout</button>
+                ) : (
+                    <button className='logoutBtn btn' onClick={gotoLogin}>Login</button>
+                )}
+            </div>
+            <div className='hamburger' onClick={() => setShow(!show)}>
+                <GiHamburgerMenu/>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
