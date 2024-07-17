52d631c990a875c750a68fbe89378a5080620541
Titu Ray choudhury <tituraychoudhury@gmail.com>
2024-07-17 23:46:48 +0530

    fetch user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
-import "./App.css"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Appointment from './pages/Appointment'
-import Register from './pages/Register'
-import Login  from './pages/Login'
-import Home from './pages/Home'
-import AboutUs from './pages/AboutUs'
-
+import React, { useContext, useEffect } from 'react';
+import "./App.css";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Appointment from './pages/Appointment';
+import Register from './pages/Register';
+import Login  from './pages/Login';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import { Context } from './main';
 
 const App = () => {
+
+  const { isAuthenticated, setIsAuthenticated, setUser } = useContext(Context);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/v1/user/patient/me",
+          {
+            withCredentials: true,
+          }
+        );
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    fetchUser();
+  }, [isAuthenticated]);
+
   return (
     <>
       <Router>
