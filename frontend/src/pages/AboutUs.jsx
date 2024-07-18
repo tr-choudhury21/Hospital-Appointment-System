3f84fd3e466dbfbfc9b337f0f2e2e0b594973fb2
Titu Ray choudhury <tituraychoudhury@gmail.com>
2024-07-19 00:09:54 +0530

    setup about us page

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import Biography from '../components/Biography'
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <>
+      <Hero title={"Learn More About Us || HeavenCare Medical Institute"} imageUrl={"/about.png"}/>
+      <Biography imageUrl={"/whoweare.png"}/>
+    </>
   )
 }
 
