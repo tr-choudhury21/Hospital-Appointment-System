a005ab207cd669c0614a9fc10b374009bb8460b9
Titu Ray choudhury <tituraychoudhury@gmail.com>
2024-07-19 00:09:43 +0530

    setup appointment page

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import AppointmentForm from '../components/AppointmentForm'
 
 const Appointment = () => {
   return (
-    <div>Appointment</div>
+    <>
+      <Hero title={"Schedule Your Appointment || HeavenCare Medical Institute"} imageUrl={"/signin.png"}/>
+      <AppointmentForm/>
+    </>
   )
 }
 
