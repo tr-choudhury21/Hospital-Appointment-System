12b385ec33b79b5a6cd95232a0fcd741b9da02a6
Titu Ray choudhury <tituraychoudhury@gmail.com>
2024-09-14 23:23:08 +0530

    add doctor authentication

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -19,8 +19,24 @@ export const isAdminAuthenticated = catchAsyncErrors(async(req, res, next) => {
     next();
 });
 
+export const isDoctorAuthenticated = catchAsyncErrors(async(req, res, next) => {
+    const token = req.cookies.doctorToken;
+
+    if(!token){
+        return next(new ErrorHandler("Admin Not Authenticated!", 400));
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    req.user = await User.findById(decoded.id);
+
+    if(req.user.role !== "Doctor"){
+        return next(new ErrorHandler(`${req.user.role} not authorized for this resource!`, 403));
+    }
+    next();
+});
+
 export const isPatientAuthenticated = catchAsyncErrors(async(req, res, next) => {
-    const token = req.cookies.patientToken;
+    const token = req.cookies.patientToken; 
 
     if(!token){
         return next(new ErrorHandler("Patient Not Authenticated!", 400));
